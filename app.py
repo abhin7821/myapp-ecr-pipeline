9f19e4eab4621b52fa853a5251cc7fcd80d1fcf4
abhin7821 <abhin7821@gmail.com>
2025-11-02 13:50:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Cloud Migration Project! 🚀"
+    return "Hello from Abhin's Auto-Synced Pipeline via ArgoCD! 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
